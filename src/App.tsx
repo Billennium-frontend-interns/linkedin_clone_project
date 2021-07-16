aff7aab47464d2716b0e3fe7dc9121234ee3ee8a
Mateusz Szklarczyk <Mateusz.Szklarczyk@billennium.com>
2021-07-16 09:19:26 +0200

    Create SignUp form

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,10 @@
 import React from 'react';
+import SignupForm from './components/SignupForm/SignupForm';
 
-const App: React.FC = () => <h1>hello world</h1>;
-
+const App: React.FC = () => (
+  <>
+    <h1>hello world</h1>
+    <SignupForm />
+  </>
+);
 export default App;
--- src/components/SignupForm/SignupForm.tsx
+++ src/components/SignupForm/SignupForm.tsx
@@ -1,5 +1,42 @@
 import React, { useState } from 'react';
+import { Button, TextField } from '@material-ui/core';
+import { auth } from '../../firebase';
 
-const SignupForm: React.FC = () => <div />;
+interface SignUpFormData {
+  name: string;
+  email: string;
+  password: string;
+  repeatPassword: string;
+}
+
+const SignupForm: React.FC = () => {
+  const initialFormData: SignUpFormData = {
+    name: '',
+    email: '',
+    password: '',
+    repeatPassword: ''
+  };
+
+  const [formData, setFormData] = useState(initialFormData);
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleSubmit = (e: React.ChangeEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log(formData);
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <TextField label="Email" type="email" value={formData.email} onChange={handleChange} />
+      <TextField label="Password" type="password" value={formData.email} onChange={handleChange} />
+      <TextField label="Repeart password" type="password" value={formData.email} onChange={handleChange} />
+      <TextField label="Nickname" type="text" value={formData.email} onChange={handleChange} />
+    </form>
+  );
+};
 
 export default SignupForm;
