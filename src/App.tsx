5f9b0cac10378976811146c8d4c964e217e60991
Mateusz Szklarczyk <Mateusz.Szklarczyk@billennium.com>
2021-07-16 14:05:06 +0200

    Remove SignupForm from App

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,8 @@
 import React from 'react';
-import SignupForm from './components/SignupForm/SignupForm';
 
 const App: React.FC = () => (
   <>
     <h1>hello world</h1>
-    <SignupForm />
   </>
 );
 export default App;
