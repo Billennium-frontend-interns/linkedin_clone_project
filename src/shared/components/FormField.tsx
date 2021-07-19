c307a43f234b336941192a9f4ab7e7f3f0586e72
Mateusz Szklarczyk <Mateusz.Szklarczyk@billennium.com>
2021-07-19 13:48:48 +0200

    Export FormField interface in FormField component, Add FormField to stories

--- src/shared/components/FormField.tsx
+++ src/shared/components/FormField.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { TextField } from '@material-ui/core';
 
-interface FormFieldInterface {
+export interface FormFieldInterface {
   label: string;
   type: string;
   name: string;
