e98397a6b96180a7ef74c163c4b880947a2d274e
Mateusz Szklarczyk <Mateusz.Szklarczyk@billennium.com>
2021-07-19 10:03:09 +0200

    Remove const formFieldConfig

--- src/components/SignupForm/useFormFieldsConfig.tsx
+++ src/components/SignupForm/useFormFieldsConfig.tsx
@@ -9,43 +9,40 @@ const useFormFieldsConfig = (
     password: boolean;
     repeatPassword: boolean;
   }
-): FormFieldConfigInterface[] => {
-  const formFieldsConfig = [
-    {
-      label: 'Email',
-      type: 'email',
-      name: 'email',
-      isError: formData.email === '' && errorMessageVisible.email,
-      helperText: formData.email === '' && errorMessageVisible.email ? "Email can't be empty" : '',
-      value: formData.email
-    },
-    {
-      label: 'Password',
-      type: 'password',
-      name: 'password',
-      isError: formData.password === '' && errorMessageVisible.password,
-      helperText: formData.password === '' && errorMessageVisible.password ? "Password can't be empty" : '',
-      value: formData.password
-    },
-    {
-      label: 'Repeat password',
-      type: 'password',
-      name: 'repeatPassword',
-      isError: formData.repeatPassword === '' && errorMessageVisible.repeatPassword,
-      helperText:
-        formData.repeatPassword === '' && errorMessageVisible.repeatPassword ? "Repeat Password can't be empty" : '',
-      value: formData.repeatPassword
-    },
-    {
-      label: 'Nickname',
-      type: 'text',
-      name: 'name',
-      isError: formData.name === '' && errorMessageVisible.name,
-      helperText: formData.name === '' && errorMessageVisible.name ? "Nickname can't be empty" : '',
-      value: formData.name
-    }
-  ];
-  return formFieldsConfig;
-};
+): FormFieldConfigInterface[] => [
+  {
+    label: 'Email',
+    type: 'email',
+    name: 'email',
+    isError: formData.email === '' && errorMessageVisible.email,
+    helperText: formData.email === '' && errorMessageVisible.email ? "Email can't be empty" : '',
+    value: formData.email
+  },
+  {
+    label: 'Password',
+    type: 'password',
+    name: 'password',
+    isError: formData.password === '' && errorMessageVisible.password,
+    helperText: formData.password === '' && errorMessageVisible.password ? "Password can't be empty" : '',
+    value: formData.password
+  },
+  {
+    label: 'Repeat password',
+    type: 'password',
+    name: 'repeatPassword',
+    isError: formData.repeatPassword === '' && errorMessageVisible.repeatPassword,
+    helperText:
+      formData.repeatPassword === '' && errorMessageVisible.repeatPassword ? "Repeat Password can't be empty" : '',
+    value: formData.repeatPassword
+  },
+  {
+    label: 'Nickname',
+    type: 'text',
+    name: 'name',
+    isError: formData.name === '' && errorMessageVisible.name,
+    helperText: formData.name === '' && errorMessageVisible.name ? "Nickname can't be empty" : '',
+    value: formData.name
+  }
+];
 
 export default useFormFieldsConfig;
