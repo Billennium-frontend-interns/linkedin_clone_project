0d23bf93f2d94a5738ab57f220f31c224ff2f808
Michał Jasiński <Michal.Jasinski@billennium.com>
2021-07-20 08:24:12 +0200

    Add react-router configuration

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,41 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
+import { AuthContext } from './context/AuthProvider';
+import ProtectedRoute from './ProtectedRoute';
 
-const App: React.FC = () => <h1>hello world</h1>;
+const App: React.FC = () => {
+  const user = useContext(AuthContext);
+
+  return (
+    <div>
+      <Router>
+        <Switch>
+          <ProtectedRoute exact path="/feed" />
+
+          <Route exact path="/" />
+
+          <Route exact path="/signUp" />
+
+          <Route exact path="/signIn" />
+
+          <Route
+            path="/"
+            render={() => {
+              if (user === undefined) {
+                return <p>Loading...</p>;
+              }
+
+              if (user) {
+                return <Redirect to="/feed" />;
+              }
+
+              return <Redirect to="/" />;
+            }}
+          />
+        </Switch>
+      </Router>
+    </div>
+  );
+};
 
 export default App;
---
+++ src/ProtectedRoute.tsx
@@ -0,0 +1,30 @@
+import React, { useContext } from 'react';
+import { Route, Redirect, RouteProps } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import { AuthContext } from './context/AuthProvider';
+
+const ProtectedRoute: React.FC<RouteProps> = ({ exact, path, component }) => {
+  const user = useContext(AuthContext);
+
+  if (user === undefined) {
+    return <p>Loading...</p>;
+  }
+
+  if (user) {
+    return <Route exact={exact} path={path} component={component} />;
+  }
+
+  return <Redirect to="/" />;
+};
+
+ProtectedRoute.defaultProps = {
+  exact: true
+};
+
+ProtectedRoute.propTypes = {
+  exact: PropTypes.bool,
+  path: PropTypes.string.isRequired,
+  component: PropTypes.func.isRequired
+};
+
+export default ProtectedRoute;
