b4001262e8182e51ca52deb7cf2ac61b0c43cbb5
Mateusz Szklarczyk <Mateusz.Szklarczyk@billennium.com>
2021-07-23 11:24:09 +0200

    Add environment variables

--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { Route } from 'react-router-dom';
 import { FormLayout } from '../pages/FormLayout/FormLayout';
 import { Welcome } from '../pages/Welcome/Welcome';
