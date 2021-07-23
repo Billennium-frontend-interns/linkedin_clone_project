51ffbfc15851360d9e28e8f7ae340f78adcaf2ed
Mateusz Szklarczyk <Mateusz.Szklarczyk@billennium.com>
2021-07-23 16:00:45 +0200

    Create getPost to get post from firebase/ Create dummy mockup for FeedPost

--- src/components/FeedList/FeedList.tsx
+++ src/components/FeedList/FeedList.tsx
@@ -1,19 +1,43 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import { db } from '../../firebase';
 
 export const FeedList: React.FC = () => {
+  const [posts, setPosts] = useState<firebase.default.firestore.DocumentData[]>([]);
+
+  const getPosts = useCallback(async () => {
+    const snapshot = await db.collection('posts').get();
+    setPosts(snapshot.docs.map(doc => doc.data()));
+  }, []);
+
   useEffect(() => {
-    db.collection('posts')
-      .get()
-      .then(doc => {
-        // eslint-disable-next-line
-        console.log(doc);
-      });
+    getPosts();
   }, []);
 
   return (
-    <div>
+    <section>
       <h1>FeedList</h1>
-    </div>
+      {posts.map(post => {
+        const { content, avatar, displayName } = post;
+        return (
+          <div
+            style={{
+              border: '2px solid black',
+              margin: '20px'
+            }}
+          >
+            <p>{displayName}</p>
+            <img
+              src={avatar}
+              alt="Profile"
+              style={{
+                height: '100px',
+                width: '100px'
+              }}
+            />
+            <p>{content}</p>
+          </div>
+        );
+      })}
+    </section>
   );
 };
