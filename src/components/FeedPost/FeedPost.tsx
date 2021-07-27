e1967e984a86fc01cd7eb5759ec62857426c74dc
kobg <kubapoland1@gmail.com>
2021-07-27 14:29:30 +0200

    Add data-testid, add proptypes

--- src/components/FeedPost/FeedPost.tsx
+++ src/components/FeedPost/FeedPost.tsx
@@ -1,5 +1,6 @@
 import React, { useRef, useState, MutableRefObject } from 'react';
 import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
 import moment from 'moment';
 import { useIsContentOverflowing } from '../../actions/useIsContentOverflowing';
 import './FeedPost.scss';
@@ -10,9 +11,17 @@ interface FeedPostProps {
   avatar: string;
   content: string;
   timestamp: string;
+  testid?: string;
 }
 
-export const FeedPost: React.FC<FeedPostProps> = ({ uid, displayName, avatar, content, timestamp }: FeedPostProps) => {
+export const FeedPost: React.FC<FeedPostProps> = ({
+  uid,
+  displayName,
+  avatar,
+  content,
+  timestamp,
+  testid
+}: FeedPostProps) => {
   const [seeMore, setSeeMore] = useState(false);
   const ref = useRef() as MutableRefObject<HTMLParagraphElement>;
   const isContentOverflowing = useIsContentOverflowing(ref);
@@ -20,7 +29,7 @@ export const FeedPost: React.FC<FeedPostProps> = ({ uid, displayName, avatar, co
 
   return (
     <article className="feedPost__container">
-      <Link className="feedPost__user" to={`/user/${uid}`}>
+      <Link className="feedPost__user" data-testid={`"feedPost__user--${testid}`} to={`/user/${uid}`}>
         <img className="feedPost__avatar" src={avatar} alt={`${displayName}'s avatar`} />
         <p className="feedPost__name">{displayName}</p>
       </Link>
@@ -29,6 +38,7 @@ export const FeedPost: React.FC<FeedPostProps> = ({ uid, displayName, avatar, co
       </p>
       {isContentOverflowing && !seeMore && (
         <button
+          data-testid={`"feedPost__seeButton--${testid}`}
           type="button"
           className={`feedPost__seeButton feedPost__seeButton--see__${seeMore ? null : 'less'}`}
           onClick={() => {
@@ -41,4 +51,17 @@ export const FeedPost: React.FC<FeedPostProps> = ({ uid, displayName, avatar, co
       <p className="feedPost__timestamp">{timePassed}</p>
     </article>
   );
+};
+
+FeedPost.defaultProps = {
+  testid: ''
+};
+
+FeedPost.propTypes = {
+  uid: PropTypes.string.isRequired,
+  displayName: PropTypes.string.isRequired,
+  avatar: PropTypes.string.isRequired,
+  content: PropTypes.string.isRequired,
+  timestamp: PropTypes.string.isRequired,
+  testid: PropTypes.string
 };
