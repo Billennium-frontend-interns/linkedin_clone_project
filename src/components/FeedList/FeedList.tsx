ec5b9eef81d9edaa73edc51b02b8b3b8bea4adca
Mateusz Szklarczyk <Mateusz.Szklarczyk@billennium.com>
2021-07-27 14:38:16 +0200

    Created fetchMoreData function for infinite-scroll component

--- src/components/FeedList/FeedList.tsx
+++ src/components/FeedList/FeedList.tsx
@@ -1,12 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { CircularProgress } from '@material-ui/core';
 import { useGetPosts } from './useGetPosts';
 import { useGetUserFollows } from './useGetUserFollows';
 import { postFilter } from './PostsFilter';
+import { PostData } from './FeedListInterfaces';
 
 export const FeedList: React.FC = () => {
+  const [posts, setPosts] = useState<PostData[]>([]);
+  const [index, setIndex] = useState(0);
+
   const { data: userFollows, isLoading: isFollowsLoading, isError: isFollowsError } = useGetUserFollows();
-  const { data: posts, isLoading: isPostsLoading, isError: isPostsError } = useGetPosts();
+  const { data: allPosts, isLoading: isPostsLoading, isError: isPostsError } = useGetPosts();
 
   if (isFollowsLoading || isPostsLoading) {
     return <CircularProgress />;
@@ -16,7 +20,17 @@ export const FeedList: React.FC = () => {
     return <p>Error has occurred please try again...</p>;
   }
 
-  const userPosts = postFilter(userFollows, posts);
+  const userPosts = postFilter(userFollows, allPosts);
+
+  const fetchMoreData = () => {
+    if (index === 0) {
+      setPosts(userPosts.slice(0, 1));
+      setIndex(index + 1);
+      return;
+    }
+    setPosts([...userPosts.splice(0, index + 1)]);
+    setIndex(index + 1);
+  };
 
   return (
     <section>
@@ -27,6 +41,14 @@ export const FeedList: React.FC = () => {
           </p>
         </div>
       ))}
+      <button
+        type="button"
+        onClick={() => {
+          fetchMoreData();
+        }}
+      >
+        click
+      </button>
     </section>
   );
 };
