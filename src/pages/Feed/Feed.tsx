4eb57098081d58521b776d8d171e88953f0e4b05
Michał Jasiński <Michal.Jasinski@billennium.com>
2021-08-06 11:18:01 +0200

    Combined and styled Feed page

--- src/pages/Feed/Feed.tsx
+++ src/pages/Feed/Feed.tsx
@@ -3,11 +3,14 @@ import { ToastContainer } from 'react-toastify';
 import { CreatePost } from '../../components/CreatePost/CreatePost';
 import { FeedList } from '../../components/FeedList/FeedList';
 import { Header } from '../../components/Header/Header';
+import './Feed.scss';
 
-export const Feed = () => (
+export const Feed: React.FC = () => (
   <div className="feed">
     <Header />
-    <CreatePost />
+    <div className="feed__createPost">
+      <CreatePost />
+    </div>
     <FeedList />
     <ToastContainer />
   </div>
