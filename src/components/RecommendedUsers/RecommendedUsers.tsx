718de8d4dd1d0973643b4dd41227eed7f5e59a89
Michał Jasiński <Michal.Jasinski@billennium.com>
2021-08-06 09:04:44 +0200

    Add expand button in recommended users section

--- src/components/RecommendedUsers/RecommendedUsers.tsx
+++ src/components/RecommendedUsers/RecommendedUsers.tsx
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import { IconButton } from '@material-ui/core';
 import { useGetUserFollows } from '../../actions/useGetUserFollows';
 import { useGetUsers } from '../../actions/useGetUsers';
 import { User } from '../../shared/interfaces/UserInterfaces';
@@ -19,7 +21,7 @@ export const RecommendedUsers: React.FC<RecommendedUsersProps> = ({ testid }) =>
 
   const getRecommendedUsersInfo = () =>
     users
-      .filter(user => !userFollows.includes(user.id))
+      .filter(user => !userFollows.includes(user.id) && !recommendedUsers?.includes(user))
       .sort(() => Math.random() - 0.5)
       .slice(0, 8);
 
@@ -30,10 +32,15 @@ export const RecommendedUsers: React.FC<RecommendedUsersProps> = ({ testid }) =>
   return (
     <WithLoader isLoading={isLoading || isFollowsLoading}>
       <WithError isError={isFollowsError || isError} errorMessage="Error has occurred please try again...">
-        <div data-testid={testid} className="recommendedUsers">
-          {recommendedUsers?.map(({ displayName, avatar, id }) => (
-            <UserCard displayName={displayName} avatar={avatar} id={id} />
-          ))}
+        <div className="recommendedUsers">
+          <div data-testid={testid} className="recommendedUsers__wrapper">
+            {recommendedUsers?.map(({ displayName, avatar, id }) => (
+              <UserCard displayName={displayName} avatar={avatar} id={id} />
+            ))}
+          </div>
+          <IconButton onClick={() => setRecommendedUsers(recommendedUsers?.concat(getRecommendedUsersInfo()))}>
+            <ExpandMoreIcon fontSize="large" />
+          </IconButton>
         </div>
       </WithError>
     </WithLoader>
--- src/pages/Feed/Feed.tsx
+++ src/pages/Feed/Feed.tsx
@@ -3,8 +3,9 @@ import { ToastContainer } from 'react-toastify';
 import { CreatePost } from '../../components/CreatePost/CreatePost';
 import { FeedList } from '../../components/FeedList/FeedList';
 import { Header } from '../../components/Header/Header';
+import './Feed.scss';
 
-export const Feed = () => (
+export const Feed: React.FC = () => (
   <div className="feed">
     <Header />
     <CreatePost />
